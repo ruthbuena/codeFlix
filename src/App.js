d9eb426ec899df89bf0f284247bc3fb668392d03
ruthbuena <ruthbpeace@gmail.com>
2017-10-05 13:57:29 -0400

    update sign in page

--- src/App.js
+++ src/App.js
@@ -3,50 +3,50 @@ import Logo from './Logo.js';
 import './App.css';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
-var Input = React.createClass({
-	render: function() {
-		return (
-			<div className="Input">
-				<input
-					id={this.props.id}
-					autoComplete="false"
-					required
-					type={this.props.type}
-					placeholder={this.props.placeholder}
-				/>
-				<label htmlFor={this.props.id}></label>
-			</div>
-		);
-	}
-});
-
-var Modal = React.createClass({
-	render: function() {
-		return (
-			<div className="Modal">
-				<form
-					onSubmit={this.props.onSubmit}
-					className="ModalForm">
-					<Input
-						id="name"
-						type="text"
-						placeholder="Jack-Edward Oliver" />
-					<Input
-						id="username"
-						type="email"
-						placeholder="mrjackolai@gmail.com" />
-					<Input
-						id="password"
-						type="password"
-						placeholder="password" />
-					<button>
-						Log in <i className="fa fa-fw fa-chevron-right"></i>
-					</button>
-				</form>
-			</div>
-		);
-	}
-});
+// var Input = React.createClass({
+// 	render: function() {
+// 		return (
+// 			<div className="Input">
+// 				<input
+// 					id={this.props.id}
+// 					autoComplete="false"
+// 					required
+// 					type={this.props.type}
+// 					placeholder={this.props.placeholder}
+// 				/>
+// 				<label htmlFor={this.props.id}></label>
+// 			</div>
+// 		);
+// 	}
+// });
+//
+// var Modal = React.createClass({
+// 	render: function() {
+// 		return (
+// 			<div className="Modal">
+// 				<form
+// 					onSubmit={this.props.onSubmit}
+// 					className="ModalForm">
+// 					<Input
+// 						id="name"
+// 						type="text"
+// 						placeholder="Jack-Edward Oliver" />
+// 					<Input
+// 						id="username"
+// 						type="email"
+// 						placeholder="mrjackolai@gmail.com" />
+// 					<Input
+// 						id="password"
+// 						type="password"
+// 						placeholder="password" />
+// 					<button>
+// 						Log in <i className="fa fa-fw fa-chevron-right"></i>
+// 					</button>
+// 				</form>
+// 			</div>
+// 		);
+// 	}
+// });
 
 // Container
 var App = React.createClass({
@@ -64,23 +64,23 @@ var App = React.createClass({
   handleChange : function(e){
       this.setState({searchTerm : e.target.value});
   },
-  render: function() {
-    var child;
-
-  		if(this.state.mounted) {
-  			child = (<Modal onSubmit={this.handleSubmit} />);
-  		}
-
-  		return(
-  			<div className="App">
-  				<ReactCSSTransitionGroup
-  					transitionName="example"
-  					transitionEnterTimeout={500}
-  					transitionLeaveTimeout={300}>
-  						{child}
-  				</ReactCSSTransitionGroup>
-  			</div>
-  		);
+  // render: function() {
+  //   var child;
+	//
+  // 		if(this.state.mounted) {
+  // 			child = (<Modal onSubmit={this.handleSubmit} />);
+  // 		}
+	//
+  // 		return(
+  // 			<div className="App">
+  // 				<ReactCSSTransitionGroup
+  // 					transitionName="example"
+  // 					transitionEnterTimeout={500}
+  // 					transitionLeaveTimeout={300}>
+  // 						{child}
+  // 				</ReactCSSTransitionGroup>
+  // 			</div>
+  // 		);
 
     return (
       <div>
